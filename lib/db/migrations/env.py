bb506425119f8579bab101d327481464e4b593ba
Collins Opiayo <opiayocollins2@gmail.com>
2025-08-27 14:01:33 +0300

    initial commit

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the project's root directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -18,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models import Base
+from lib.db.base import Base
 target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
